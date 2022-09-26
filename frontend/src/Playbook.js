a8e1cf364aff32951564214b8b097bd342591cb6
daod-arshad <brilliance.dawood@gmail.com>
2022-09-26 12:42:13 +0500

    modified:   frontend/public/index.html 	modified:   frontend/src/Playbook.js

--- frontend/src/Playbook.js
+++ frontend/src/Playbook.js
@@ -15,14 +15,13 @@ const initialNodes = [
   }, 
   */
 ];
-
+const nodeTypes = { textUpdater: TextUpdaterNode };
 let id = 0;
 const getId = () => `dndnode_${id++}`;
 
 
 function Playbook() {
   
-  const nodeTypes = { textUpdater: TextUpdaterNode };
   const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
@@ -43,25 +42,61 @@ function Playbook() {
 
   const onDrop = useCallback(
     (event) => {
+
       var count = 0
       var startEnd = []
-       console.log(edges)
-       /* edges.map(item => {
-         console.log(`${item.source}  is connected to ${item.target}`)
-        // console.log(item.target);
-         edges.map(item1 => {
-           if (item == item1) {
-             count+=1
-           }
-         })
-
-         if (count == 1) {
-           startEnd.push(item)
-         }
-         count=0
+      var startAndEnd = []
+      var pointer;
+      var sequence = []
+      console.log(edges)
+      edges.map(item => {
+        startEnd.push(item.source)
+        startEnd.push(item.target)
+      })
+      // console.log(startEnd)
+      startEnd.map(item => {
         
-       }) 
-      console.log(`The starting and ending nodes are ${startEnd}`) */
+        startEnd.map(duplicateItem => {
+          if (item === duplicateItem) {
+            count+=1
+          }
+        })
+        if (count === 1) {
+          startAndEnd.push(item)
+        }
+        count=0
+      })
+
+      edges.map(item2 => {
+        startAndEnd.map(item3 => {
+          if (item2.source === item3) {
+            pointer = item3
+            // console.log(`Starting node is ${pointer}`)
+          }
+        })
+      })
+
+      if (edges.length === 0) {
+        console.log("There is only one Node");
+      } else if (edges.length === 1) {
+        console.log("There are only 2 nodes");
+      }
+      else {
+        sequence.push(pointer)
+        for (var i = 0; i < edges.length; i++){
+          edges.map(item => {
+            if (item.source === pointer) {
+              sequence.push(item.target)
+              pointer = item.target
+              // console.log(`The next node is ${pointer}`)
+            }
+          })
+        }  
+        console.log(sequence);
+      }
+      
+
+
       event.preventDefault();
 
       const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
