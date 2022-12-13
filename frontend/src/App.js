83cfcabde47b71d978cc55ecb29c6c48cb989e99
daod-arshad <brilliance.dawood@gmail.com>
2022-12-13 23:17:52 +0500

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/public/index.html 	modified:   frontend/src/App.js 	new file:   frontend/src/loginSignup/Login.js 	new file:   frontend/src/loginSignup/style/login.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,53 @@
 import Playbook from "./homepage/Playbook";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TestPage from "./TestPage"
+import Login from "./loginSignup/Login.js"
+import jwt_decode from "jwt-decode";
+import { useState, useEffect } from "react"
+
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  useEffect(() => {
+    try {
+      let token = localStorage.getItem("token");
+      let decodedToken = jwt_decode(token);
+      // console.log("Decoded Token", decodedToken);
+      let currentDate = new Date();
+      // JWT exp is in seconds
+      if (decodedToken.exp * 1000 < currentDate.getTime()) {
+        console.log("Token expired.");
+        setLoggedIn(false);
+      } else {
+        console.log("Valid token");
+        setLoggedIn(true);
+      }
+    }
+    catch {
+      setLoggedIn(false)
+    }
+    
+    },[]);
+  
+    
+
   return (
     <div className="app">
       <div className="app_body">
-        {/* <Playbook/> */}
         <Router>
           <Routes>
             <Route path="/">
-              <Route index element={<Playbook />} />
-              <Route path="test" element={<TestPage />} />
+              <Route index element={<Login />} />
+              {
+                loggedIn ?
+                  [
+                    <Route path="test" element={<TestPage />} />,
+                    <Route path="playbook" element={<Playbook />} />
+                  ]
+                  : null
+              }
+              {/* <Route path="test" element={<TestPage />} />
+              <Route path="playbook" element={<Playbook />} /> */}
             </Route>
           </Routes>
         </Router>
---
+++ frontend/src/loginSignup/Login.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import "./style/login.css"
+import axios from "../axios.js"
+import jwt from "jwt-decode"
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [error, setErrormsg] = useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+let navigate = useNavigate(); 
+  //functions
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name.length === 0 || password.length === 0) {
+      setErrormsg(true);
+    }else{
+        console.log("The form was submitted with the following data:");
+        console.log("username: " + name, "password: " + password);
+        axios
+          .post("/user/login", {
+            username: name, password: password
+          })
+            .then((response) => {
+                    // console.log("Login alert");
+                // console.log(response.data);
+                // const user = jwt(response.data)
+                // console.log(user)
+                localStorage.setItem("token", response.data)
+                navigate("/playbook")
+      
+                }, []).
+            catch(err => console.log("Incorrect Username or Password"));
+    }
+  };
+
+  //    //Validate Password Function
+  // const validate = (password) => {
+
+  //   if (validator.isStrongPassword(password, {
+  //     minLength: 8, minLowercase: 1,
+  //     minUppercase: 1, minNumbers: 1, minSymbols: 1
+  //   })) {
+  //     setErrorMessage('')
+  //     setPassword(password);
+  //   } else {
+  //     setErrorMessage('Is Not Strong Password')
+  //   }
+  // }
+
+  return (
+    <>
+      <div className="formCenter">
+        <h3 className="formTitle">Sign In</h3>
+        <br />
+        <form className="formFields" onSubmit={handleSubmit}>
+          <div className="formFields">
+            <label className="formFieldLabel" htmlFor="name">
+              Username
+            </label>
+            <input
+              type="username"
+              id="username"
+              className="formFieldInput"
+              placeholder="Enter your name"
+              name="username"
+              //value={state.email}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <br />
+            <div style={{ height: "40px" }}>
+              {error && name.length <= 0 ? (
+                <div className="label">Username can't be empty</div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+
+          <div className="formField">
+            <label className="formFieldLabel" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="formFieldInput"
+              placeholder="Enter your password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <div style={{ height: "45px" }}>
+              {error && password.length <= 0 ? (
+                <div className="label">Password can't be empty</div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+
+          <div className="formField">
+            <button
+              className="formFieldButton"
+              style={{ display: "block", margin: "0 auto" }}
+              onClick={handleSubmit}
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
