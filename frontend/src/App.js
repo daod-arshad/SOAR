8c8b4df756d0eeceef1de4f0daa07d5d6a170b39
daod-arshad <brilliance.dawood@gmail.com>
2022-12-23 09:26:59 +0500

    modified:   frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,31 +9,31 @@ import React ,{ useState, useEffect } from "react"
 
 function App() {
   // let navigate = useNavigate(); 
-  let id = 0;
-  const getId = () => id++;
-  const [loggedIn, setLoggedIn] = useState(false)
-  useEffect(() => {
-    try {
-      let token = localStorage.getItem("token");
-      let decodedToken = jwt_decode(token);
-      // console.log("Decoded Token", decodedToken);
-      let currentDate = new Date();
-      // JWT exp is in seconds
-      if (decodedToken.exp * 1000 < currentDate.getTime()) {
-        console.log("Token expired.");
-        setLoggedIn(false);
-      } else {
-        console.log("Valid token");
-        setLoggedIn(true);
-        redirect("/playbook")
-      }
-    }
-    catch {
-      setLoggedIn(false)
-    }
-    console.log(loggedIn)
+  // let id = 0;
+  // const getId = () => id++;
+  // const [loggedIn, setLoggedIn] = useState(false)
+  // useEffect(() => {
+  //   try {
+  //     let token = localStorage.getItem("token");
+  //     let decodedToken = jwt_decode(token);
+  //     // console.log("Decoded Token", decodedToken);
+  //     let currentDate = new Date();
+  //     // JWT exp is in seconds
+  //     if (decodedToken.exp * 1000 < currentDate.getTime()) {
+  //       console.log("Token expired.");
+  //       setLoggedIn(false);
+  //     } else {
+  //       console.log("Valid token");
+  //       setLoggedIn(true);
+  //       redirect("/playbook")
+  //     }
+  //   }
+  //   catch {
+  //     setLoggedIn(false)
+  //   }
+  //   console.log(loggedIn)
     
-    },[loggedIn]);
+  //   },[loggedIn]);
   
     
 
@@ -45,18 +45,18 @@ function App() {
             <Route path="/">
               <Route index element={<Login />} />
 
-              {loggedIn ? (
-                [
-                  <React.Fragment key={getId()}>
-                    <Route path="test" element={<TestPage />} />,
-                    <Route path="signup" element={<Signup />} />,
-                    <Route path="playbook" element={<Playbook />} />,
-                    <Route path="*" element={<h1>Path not Resolved</h1>} />,
-                  </React.Fragment>
-                ]
-              ) : (
-                <Route path="*" element={<Navigate to="/" replace/>} />
-              )}
+              {/* {loggedIn ? ( */}
+                {/* [ */}
+                  {/* <React.Fragment key={getId()}> */}
+                    <Route path="test" element={<TestPage />} />
+                    <Route path="signup" element={<Signup />} />
+                    <Route path="playbook" element={<Playbook />} />
+                    <Route path="*" element={<h1>Path not Resolved</h1>} />
+                  {/* </React.Fragment> */}
+                {/* ] */}
+              {/* ) : ( */}
+                {/* <Route path="*" element={<Navigate to="/" replace/>} /> */}
+              {/* )} */}
             </Route>
           </Routes>
         </Router>
